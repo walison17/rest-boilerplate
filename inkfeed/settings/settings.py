ef746c5525a4dadbda445ab765d907ec61686902
Sergio Mattei <7959397+matteing@users.noreply.github.com>
2019-03-07 23:22:37 -0400

    Update settings.py

--- inkfeed/settings/settings.py
+++ inkfeed/settings/settings.py
@@ -119,12 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "../../assets")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "../../static"),
+    os.path.join(BASE_DIR, "static"),
 ]
-
 
 AUTH_USER_MODEL = 'accounts.User'
 
